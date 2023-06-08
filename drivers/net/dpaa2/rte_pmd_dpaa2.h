33e8ed1b016e1e0c5068b6e79acab60a2172843b
Sachin Saxena <sachin.saxena@oss.nxp.com>
2023-06-08 18:27:00 +0530

    Add dpaa2 worker thread warmup api interface
     - Application should call this API at first place in worker
       thread to avoid initial runtime delays during I/O.
     - For PMU based benchmarking, calling this API is must
       before we enable PMU access.
     - rx_flag should be enabled if thread is designed to do
       dpaa2 packet receive handling.
    
    Signed-off-by: Sachin Saxena <sachin.saxena@oss.nxp.com>
    DPDK-3870

--- drivers/net/dpaa2/rte_pmd_dpaa2.h
+++ drivers/net/dpaa2/rte_pmd_dpaa2.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright 2018-2022 NXP
+ * Copyright 2018-2023 NXP
  */
 
 #ifndef _RTE_PMD_DPAA2_H
@@ -112,5 +112,16 @@ rte_pmd_dpaa2_get_tlu_hash(uint8_t *key, int size);
 __rte_experimental
 int
 rte_pmd_dpaa2_set_opr(uint16_t port_id, uint16_t rx_queue_id);
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change, or be removed, without prior notice
+ *
+ * Do thread specific I/O initialization & warmup to reduce initial delays
+ *
+ */
+__rte_experimental
+int
+rte_pmd_dpaa2_thread_warmup(uint8_t rx_flag);
 
 #endif /* _RTE_PMD_DPAA2_H */
